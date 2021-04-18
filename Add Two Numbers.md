dc41b310b1fb6b5336dfab8a1b8e29adb20c28dc
jpark143 <jpark143@g.ucla.edu>
2021-04-18 13:17:09 +0900

    no message

---
+++ Add Two Numbers.md
@@ -0,0 +1,5 @@
+![node](/image/node.png)
+
+- Solution 1 : parameter c refers to carry
+- Solution 2 : divmod generates two parameters, quotient and remainder, 
+<br/> for which val // 10 and val % 10 in Solution 1
---
+++ Add Two Numbers.py
@@ -0,0 +1,48 @@
+"""Solution 1"""
+
+class Solution:
+    def addTwoNumbers(self, l1, l2, c=0):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        val = l1.val + l2.val + c
+        c = val // 10
+        ret = ListNode(val % 10)
+
+        if (l1.next != None or l2.next != None or c != 0):
+            if l1.next == None:
+                l1.next = ListNode(0)
+            if l2.next == None:
+                l2.next = ListNode(0)
+            ret.next = self.addTwoNumbers(l1.next, l2.next, c)
+        return ret
+
+
+"""Solution 2"""
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            carry, out = divmod(val1 + val2 + carry, 10)
+
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
